564fcd2cf4a334d1bf6fb5abdc7f9c378ac9dbf0
aditisonik01 <asonik@asu.edu>
2017-04-27 23:54:21 -0700

    KB by wordnet
    wordnet_match with some changes for python 2.7

---
+++ DataModelling/isaRuleBuilder_wordNet.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+# Result is in this format--
+isa(<word-meaning from wordnet>,<word picked from document>)
+"""
+import os
+from wordnet_match_27 import wordnet_match
+    
+dir = os.path.dirname(__file__)
+path =  dir + '/ReducedRuleFiles'
+outputPath = dir + '/CompleteKB'
+isAKB = open(outputPath + "/CompleteKB.txt", 'w')
+for filename in os.listdir(path):
+    print (filename)      
+    inFile = open(path + "/" + filename,'r')
+    entities = [x.split(' | ')[2].strip() for x in inFile.readlines()]
+    
+    isAKB.write('Rules for file: ' + filename + '-\n \n')  
+    for entity in entities:
+        #print (entity)
+        try :
+            priory_meaning_match = wordnet_match(entity)
+        except IndexError:
+            continue 
+        except UnicodeDecodeError:
+            continue
+        #writting terms to txt-file
+        for item in priory_meaning_match[0]:
+            names = item.lemma_names()
+            for meaning in names:
+                #print (meaning,names[0])
+                isAKB.write('isa(' + meaning.encode('utf-8') + ',' + names[0].encode('utf-8') + ')\n')
+    isAKB.write('\n')
+    inFile.close()
+    #break
+
+#query = raw_input("search query:")
+#print (priory_meaning_match)
+
+isAKB.close()
+
+    
---
+++ DataModelling/wordnet_match_27.py
@@ -0,0 +1,90 @@
+from nltk.corpus import wordnet as wn
+from collections import Counter
+
+
+def wordnet_match(query):
+    
+    query_terms = query.split()
+    involved_terms = Counter(query_terms)
+    involved_meanings = []
+    priory_meanings = []
+    
+    #print(involved_terms)
+    
+    for term in involved_terms:
+        involved_meanings = involved_meanings + wn.synsets(term)
+        priory_meanings.append(wn.synsets(term))
+    
+    #priory_meanings.remove([])
+    #print('-----involved meanings-----')
+    #print(involved_meanings)
+    #print('-----priory meanings-----')
+    #print(priory_meanings)
+    
+    cleared_term = []
+    while not len(priory_meanings) == 1:
+        max_simi = -1
+        max_pair = []
+        max_meanings = []
+        for term1 in priory_meanings:
+            for term2 in priory_meanings:
+                if not term1 is term2:
+                    max_term_simi = -1
+                    max_term_pair = []
+                    for meaning1 in term1:
+                        for meaning2 in term2:
+                            if max_term_simi < meaning1.path_similarity(meaning2):
+                                max_term_simi = meaning1.path_similarity(meaning2)
+                                max_term_pair = [term1, term2]
+                                max_meaning_pair = [meaning1, meaning2]
+    
+                    if max_simi < max_term_simi:
+                        max_simi = max_term_simi
+                        max_pair = max_term_pair
+                        max_meanings = max_meaning_pair
+    
+        #print('-----max pair-----')
+        #print (max_pair)
+        #print('-----max meanings-----')
+        #print (max_meanings)
+    
+        if max_pair[0] in cleared_term and max_pair[1] in cleared_term:
+            new_term = max_pair[0] + max_pair[1]
+            cleared_term.remove(max_pair[0])
+            cleared_term.remove(max_pair[1])
+        elif max_pair[0] in cleared_term:
+            max_pair[0].append(max_meanings[1])
+            new_term = max_pair[0]
+            cleared_term.remove(max_pair[0])
+        elif  max_pair[1] in cleared_term:
+            max_pair[1].append(max_meanings[0])
+            new_term = max_pair[1]
+            cleared_term.remove(max_pair[1])
+        else:
+            new_term = max_meanings
+        cleared_term.append(new_term)
+        priory_meanings.remove(max_pair[0])
+        priory_meanings.remove(max_pair[1])
+        priory_meanings.append(new_term)
+        #print('-----cleared term-----')
+        #print(cleared_term)
+        #print('-----priory meaning-----')
+        #print(priory_meanings)
+    return priory_meanings
+
+
+#query = raw_input("search query:")
+
+#priory_meaning_match = wordnet_match(query)
+
+
+
+
+
+
+
+
+
+
+
+
