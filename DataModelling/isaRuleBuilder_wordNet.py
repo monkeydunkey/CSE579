3150becc6abcdb2aa72b4f712f8d41215d1ee180
aditisonik01 <asonik@asu.edu>
2017-04-30 01:29:52 -0700

    minor fixes
    removing redundancy and irrelevant terms

--- DataModelling/isaRuleBuilder_wordNet.py
+++ DataModelling/isaRuleBuilder_wordNet.py
@@ -11,43 +11,55 @@ print dir
 path =  dir + '/ReducedRuleFiles'
 outputPath = dir + '/CompleteKB'
 isAKB = open(outputPath + "/CompleteKB_WN.txt", 'w')
+query_seen = set()
 for filename in os.listdir(path):
     print (filename)      
     inFile = open(path + "/" + filename,'r')
     entities = [x.split(' | ')[2].strip() for x in inFile.readlines()]    
     #isAKB.write('Rules for file: ' + filename + '-\n \n')
+    
+    #query_processed = []
     for entity in entities:
-        try :
-            priory_meaning_match = wordnet_match(entity)
-        except IndexError:
-            continue 
-        except UnicodeDecodeError:
+        if entity not in query_seen:            
+            query_seen.add(entity)
+            try :
+                priory_meaning_match = wordnet_match(entity)
+            except IndexError:
+                continue 
+            except UnicodeDecodeError:
+                continue
+            #writting terms to txt-file
+            if len(priory_meaning_match[0]) > 1:            
+                meaningList = list()
+                for item in priory_meaning_match[0]:
+                    names = item.lemma_names()
+                    meaningList.append(names[0])
+                
+                #formatting
+                meaningList = [x.encode('utf-8') for x in meaningList]
+                seen = set()
+                result = []
+                for i in meaningList:
+                    if i not in seen:
+                        seen.add(i)
+                        result.append(i)        
+                #print result
+                #break
+                meaningList = result
+                meaningList = " ".join(str(x) for x in meaningList)
+                
+                #print (meaningList)
+                isAKB.write('isa(' + unicode(entity, "utf-8") + ',' + meaningList + ')\n')
+                #isAKB.write('\n')
+                inFile.close()
+                #break
+            else:
+                #print priory_meaning_match
+                continue
+            #break        
+        else:
+            #print entity
             continue
-        #writting terms to txt-file
-        meaningList = list()
-        for item in priory_meaning_match[0]:
-            names = item.lemma_names()
-            meaningList.append(names[0])
-        
-        #formatting
-        meaningList = [x.encode('utf-8') for x in meaningList]
-        seen = set()
-        result = []
-        for i in meaningList:
-            if i not in seen:
-                seen.add(i)
-                result.append(i)        
-        #print result
-        #break
-        meaningList = result
-        meaningList = " ".join(str(x) for x in meaningList)
-        
-        #print (meaningList)
-        isAKB.write('isa(' + unicode(entity, "utf-8") + ',' + meaningList + ')\n')
-        #isAKB.write('\n')
-        inFile.close()
-        
-
 isAKB.close()
 
     
