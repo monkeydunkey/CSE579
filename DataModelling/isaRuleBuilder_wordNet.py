298c5f45432ac39b26de7ee2445a9473c8b0e1d8
aditisonik01 <asonik@asu.edu>
2017-04-29 22:37:04 -0700

    wordnet code minor changes

--- DataModelling/isaRuleBuilder_wordNet.py
+++ DataModelling/isaRuleBuilder_wordNet.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 # Result is in this format--
-isa(<word-meaning from wordnet>,<word picked from document>)
+isa(<word picked from document>,<word-meaning from wordnet>)
 """
 import os
 from wordnet_match_27 import wordnet_match
@@ -10,15 +10,13 @@ dir = os.getcwd()#os.path.dirname(__file__)
 print dir
 path =  dir + '/ReducedRuleFiles'
 outputPath = dir + '/CompleteKB'
-isAKB = open(outputPath + "/CompleteKB.txt", 'w')
+isAKB = open(outputPath + "/CompleteKB_WN.txt", 'w')
 for filename in os.listdir(path):
     print (filename)      
     inFile = open(path + "/" + filename,'r')
-    entities = [x.split(' | ')[2].strip() for x in inFile.readlines()]
-    
-    isAKB.write('Rules for file: ' + filename + '-\n \n')  
+    entities = [x.split(' | ')[2].strip() for x in inFile.readlines()]    
+    #isAKB.write('Rules for file: ' + filename + '-\n \n')
     for entity in entities:
-        #print (entity)
         try :
             priory_meaning_match = wordnet_match(entity)
         except IndexError:
@@ -26,17 +24,29 @@ for filename in os.listdir(path):
         except UnicodeDecodeError:
             continue
         #writting terms to txt-file
+        meaningList = list()
         for item in priory_meaning_match[0]:
             names = item.lemma_names()
-            for meaning in names:
-                #print (meaning,names[0])
-                isAKB.write('isa(' + meaning.encode('utf-8') + ',' + names[0].encode('utf-8') + ',' + entity.encode('utf-8') + ')\n')
-    isAKB.write('\n')
-    inFile.close()
-    #break
-
-#query = raw_input("search query:")
-#print (priory_meaning_match)
+            meaningList.append(names[0])
+        
+        #formatting
+        meaningList = [x.encode('utf-8') for x in meaningList]
+        seen = set()
+        result = []
+        for i in meaningList:
+            if i not in seen:
+                seen.add(i)
+                result.append(i)        
+        #print result
+        #break
+        meaningList = result
+        meaningList = " ".join(str(x) for x in meaningList)
+        
+        #print (meaningList)
+        isAKB.write('isa(' + unicode(entity, "utf-8") + ',' + meaningList + ')\n')
+        #isAKB.write('\n')
+        inFile.close()
+        
 
 isAKB.close()
 
